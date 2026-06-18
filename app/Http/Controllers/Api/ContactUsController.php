1c9a38175af8d8f1604ef589132b0634a6104a62
Muhammad Atif <matifch16@gmail.com>
2026-06-18 15:53:38 +0500

    contact us some update

--- app/Http/Controllers/Api/ContactUsController.php
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -2,25 +2,33 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\User;
-use App\Models\ContactUs;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ContactUsRequest;
+use App\Models\ContactUs;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class ContactUsController extends Controller
 {
     public function index(ContactUsRequest $request)
     {
-        $deviceId = request()->header('Device-Id');
-        $user = User::where('device_id', $deviceId)->first();
-        
-        $data = $request->validated();
-        $data['user_id'] = $user ? $user->id : null;
+        try {
+            $deviceId = request()->header('Device-Id');
+            $user = User::where('device_id', $deviceId)->first();
 
-        ContactUs::create($data);
+            $data = $request->validated();
+            $data['user_id'] = $user ? $user->id : null;
 
-        // Return the messages as a JSON response
-        return response()->json(['data' => $data, 'success' => 'Data submit successfully'], 200);
+            ContactUs::create($data);
+
+            Log::info('contact us data create successfully');
+            // Return the messages as a JSON response
+            return response()->json(['data' => $data, 'success' => 'Data submit successfully'], 200);
+        } catch (\Exception $e) {
+            // Handle any exceptions that may occur
+            Log::error('Error submitting contact us form: ' . $e->getMessage());
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+
     }
 }
